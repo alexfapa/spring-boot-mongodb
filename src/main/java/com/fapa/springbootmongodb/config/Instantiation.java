a01c237be075fe837f6785a5d75c0342915278f3
Alex Pinto <alexfapa32@gmail.com>
2021-01-28 16:23:18 -0300

    Instaciando DTO

---
+++ src/main/java/com/fapa/springbootmongodb/config/Instantiation.java
@@ -0,0 +1,32 @@
+package com.fapa.springbootmongodb.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import com.fapa.springbootmongodb.domain.User;
+import com.fapa.springbootmongodb.repository.UserRepository;
+
+@Configuration
+public class Instantiation implements CommandLineRunner {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		userRepository.deleteAll();
+		
+		User maria = new User(null, "Maria Brown", "maria@gmail.com");
+		User alex = new User(null, "Alex Green", "alex@gmail.com");
+		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		User alexpinto = new User(null, "Alex Pinto", "alexfapa32@gmail.com");
+		
+		userRepository.saveAll(Arrays.asList(maria, alex, bob, alexpinto));
+		
+		
+	}
+
+}
