0ccd0a3faff8a801595bededdbcd83a0d1a50a6b
Alex Pinto <alexfapa32@gmail.com>
2021-01-30 19:47:01 -0300

    Adição do método update em UserService e UserResource

--- src/main/java/com/fapa/springbootmongodb/service/UserService.java
+++ src/main/java/com/fapa/springbootmongodb/service/UserService.java
@@ -3,6 +3,8 @@ package com.fapa.springbootmongodb.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.mongodb.core.schema.TypedJsonSchemaObject.BooleanJsonSchemaObject;
 import org.springframework.stereotype.Service;
 
 import com.fapa.springbootmongodb.domain.User;
@@ -37,6 +39,19 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		findById(obj.getId());
+		User newObj = obj;
+		updateData(newObj, obj);
+		return repo.save(newObj);
+		
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());		
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
