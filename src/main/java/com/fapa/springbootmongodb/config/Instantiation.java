2799ebb3b65c99447894658c480e63e60b738a3c
Alex Pinto <alexfapa32@gmail.com>
2021-02-01 16:32:53 -0300

    Adição de referenciamento dos posts para usuários

--- src/main/java/com/fapa/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/fapa/springbootmongodb/config/Instantiation.java
@@ -42,7 +42,11 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, stf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
 		Post post2 = new Post(null, stf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 	
-		postRepository.saveAll(Arrays.asList(post1, post2));	
+		postRepository.saveAll(Arrays.asList(post1, post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		
+		userRepository.save(maria);
 		
 	}
 
