ef07ff2f8c16b9abd387bc0955f6c54e7dc6cd3a
Alex Pinto <alexfapa32@gmail.com>
2021-02-01 14:37:38 -0300

    Criação da classo AuthorDTO e implementação dos métodos respectivos à classe

--- src/main/java/com/fapa/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/fapa/springbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.fapa.springbootmongodb.domain.Post;
 import com.fapa.springbootmongodb.domain.User;
+import com.fapa.springbootmongodb.dto.AuthorDTO;
 import com.fapa.springbootmongodb.repository.PostRepository;
 import com.fapa.springbootmongodb.repository.UserRepository;
 
@@ -29,16 +30,18 @@ public class Instantiation implements CommandLineRunner {
 		stf.setTimeZone(TimeZone.getTimeZone("GMT"));
 		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		User alexpinto = new User(null, "Alex Pinto", "alexfapa32@gmail.com");
 		
-		Post post1 = new Post(null, stf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", maria);
-		Post post2 = new Post(null, stf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob, alexpinto));
+		
+		Post post1 = new Post(null, stf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo", new AuthorDTO(maria));
+		Post post2 = new Post(null, stf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+	
 		postRepository.saveAll(Arrays.asList(post1, post2));	
 		
 	}
