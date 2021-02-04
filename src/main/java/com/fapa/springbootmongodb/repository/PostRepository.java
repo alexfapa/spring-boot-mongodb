1996a65c9bce9dbf03bd6d8fcf15a816b9eeb9b7
Alex Pinto <alexfapa32@gmail.com>
2021-02-04 11:03:02 -0300

    Adicionando busca de posts com @Query

--- src/main/java/com/fapa/springbootmongodb/repository/PostRepository.java
+++ src/main/java/com/fapa/springbootmongodb/repository/PostRepository.java
@@ -3,12 +3,19 @@ package com.fapa.springbootmongodb.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fapa.springbootmongodb.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
+	//title -> campo que será referenciado
+	//$regex -> o número relacionado ao campo que será mostrado, no caso text que é o primeiro campo
+	//$options -> no caso i para ignorar letras maiúsculas
+	@Query("{'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
